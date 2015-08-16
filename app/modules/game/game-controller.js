28c1d9e67c8c2e0a4f0b0beef856e6067af24f55
Yigit Erol <yerol@thunderhead.com>
2015-08-16 20:24:51 +0100

    Implemented the application console window and enabled accepting user commands.

--- app/modules/game/game-controller.js
+++ app/modules/game/game-controller.js
@@ -1,12 +1,48 @@
 // Main controller for the Game module
-angular.module('Modules.Game')
+angular.module('Modules.Game', [])
 	.controller('Modules.Game.Controller', [function () {
 		'use strict';
+
+		this.model = {
+			userInput : '',
+			consoleText : 'Angular Mini ZORK\n' +
+						  'Copyright (c) 2015 Yigit Erol. All rights reserved.\n' +
+						  'ZORK is a registered trademark of Infocom Inc.\n' +
+						  'Revision 1.\n' +
+						  '\n' +
+						  'Supported Commands:\n' +
+						  'look at room\n' +
+						  'look at me\n' +
+						  'say [message]\n' +
+						  '\n' +
+						  'Begin..'
+		};
+
+		this.executeCommand = function() {
+			this.model.consoleText += '\n>' + this.model.userInput;
+			this.model.userInput = '';
+		};
+
+		this.executeInput = function($event) {
+			// Execute on enter
+			if ($event.keyCode === 13 && this.model.userInput.length > 0) {
+				this.executeCommand(this.model.userInput);
+				$event.preventDefault();
+			}
+			// Prevent tabbing away
+			else if ($event.keyCode === 9) {
+				$event.preventDefault();
+			}
+			// Prevent browser history back on backspace when input is empty
+			// TODO: The input loses focus at this point somehow. Figure out why.
+			else if($event.keyCode === 8 && this.model.userInput.length === 0) {
+				$event.preventDefault();
+			}
+		};
 
 		/*
 		 * Destroy handler for the controller
 		 */
 		this.onDestroy = function () {
-
 		};
 	}]);
--- app/modules/game/game-directive.js
+++ app/modules/game/game-directive.js
@@ -1,8 +1,7 @@
 // Creating the Game module and defining the Game directive
 // which bridges the template and controller without depending on ng-controller (using controllerAs syntax)
-angular.module('Modules.Game', [])
-	.directive('game', [
-	function () {
+angular.module('Modules.Game')
+	.directive('game', ['$window', function ($window) {
 		'use strict';
 
 		return {
@@ -10,6 +9,26 @@ angular.module('Modules.Game', [])
 			controllerAs: 'gameController',
 			controller: 'Modules.Game.Controller',
 			link: function (scope, element, attrs, ctrl) {
+				var my = {
+					// Cache the input element
+					// Set the initial  focus on the input to accept keyboard input
+					input : element.find('.game-input').focus(),
+					// Cache the console textarea
+					textarea : element.find('.game-console-textbox')
+				};
+
+				my.input.on('blur', function() {
+					my.input.focus();
+				});
+
+				angular.element($window).on('click', function() {
+					my.input.focus();
+				});
+
+				scope.$watch(function() { return ctrl.model.userInput; }, function() {
+					my.textarea.scrollTop(my.textarea[0].scrollHeight);
+				});
+
 				// Call controller's destroy event on scope.$destroy so we can clean up any resources used/created
 				scope.$on('$destroy', ctrl.onDestroy);
 			}
